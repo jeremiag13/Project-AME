cd9f9bde4b1122f9e4021fd4b8b288b5c4c2d81e
Jeremia Gondowijoyo <57291054+jeremiag13@users.noreply.github.com>
2026-04-01 23:56:31 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,8 @@
-<script src="https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.0.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore-compat.js"></script>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
